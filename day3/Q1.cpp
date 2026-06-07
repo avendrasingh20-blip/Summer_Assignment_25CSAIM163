3bdd4df0df8b09ce8c3f800f045ceecacd3ccad9
Avendra Singh <avendrasingh20@gmail.com>
2026-06-07 20:34:15 +0530

    Added day3 assignment

---
+++ day3/Q1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+bool isPrime(int n)
+{
+    if (n<=1) return false;
+    for (int i = 2; i <= sqrt(n); i++)
+    {
+        if(n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+int main()
+{
+    int num;
+    cout << "Enter a number:";
+    cin >> num;
+
+    if (isPrime(num))
+    {
+        cout << num << " ia a prime number.\n";
+    }else{
+        cout << num << " is not a prime number.\n";
+    }
+    return 0;
+}
