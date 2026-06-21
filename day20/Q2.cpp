10b7ac9b6ed509f063de3fb759e7f39ceef781c4
Avendra Singh <avendrasingh20@gmail.com>
2026-06-22 01:54:19 +0530

    Added code file day18 to day21

---
+++ day20/Q2.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+ {
+    int n;
+    cout << "Enter order of square matrix: ";
+    cin >> n;
+
+    int A[n][n];
+   
+    bool symmetric = true;
+
+   
+    cout << "Enter matrix elements:\n";
+   
+    for (int i = 0; i < n; i++)
+       
+    for (int j = 0; j < n; j++)
+          
+    cin >> A[i][j];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) 
+        
+        {
+
+            if (A[i][j] != A[j][i]) 
+           
+            {
+
+                symmetric = false;
+                break;
+            }
+        }
+    }
+
+    if (symmetric)
+        cout << "Matrix is Symmetric";
+    else
+        cout << "Matrix is Not Symmetric";
+
+    return 0;
+}
